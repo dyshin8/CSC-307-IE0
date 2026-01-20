5af78f130b99d683428c1ad13f240381fc0a79e5
Don Shin <dongyunshin8@gmail.com>
2026-01-20 15:23:06 -0800

    Added the ability to post users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 const app = express();
 const port = 8000;
 
@@ -67,6 +72,12 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
