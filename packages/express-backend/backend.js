e4a7edda598ef4d3bbff8fdd31a97db5dc33ac68
Don Shin <dongyunshin8@gmail.com>
2026-01-20 15:11:21 -0800

    Added ID Access

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,6 +36,9 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 const app = express();
 const port = 8000;
 
@@ -53,6 +56,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
